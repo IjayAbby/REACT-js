e6243db6da7d5b2d83c44e11673a5d47530a7c41
IjayAbby <inyangasi4@gmail.com>
2020-02-28 14:43:05 +0300

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 //Creating a function component called pet
 const Pet = (props) => { 
-    return React.createElement("div",null[
-        React.createElement("h1",null,props.name),
-        React.createElement("h2",null,props.animal),
-        React.createElement("h2",null,props.breed)
+    return React.createElement("div", null, [
+        React.createElement("h1", null, props.name),
+        React.createElement("h2", null, props.animal),
+        React.createElement("h2", null, props.breed)
     ])
 }
 
 const App = () => {
-    return React.createElement("div",null,[
-        React.createElement("h1",null,"Adopt Me"),
+    return React.createElement("div", null,[
+        React.createElement("h1", null,"Adopt Me"),
         React.createElement(Pet,{
             name : "Luna",
-            animal :"Dog",
-            breed: "Havanese"
+            animal : "Dog",
+            breed : "Havanese"
         }),
         React.createElement(Pet,{
             name : "Pepper",
-            animal :"Hen",
-            breed: "Bird"
+            animal :"Bird",
+            breed : "Cockatiel"
         }),
         React.createElement(Pet,{
             name : "Doink",
-            animal :"Cat",
-            breed: "Mix"
+            animal : "Cat",
+            breed : "Mix"
         })
     ])
 }
