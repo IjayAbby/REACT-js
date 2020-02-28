b4c7d794da395d7bf23e0b6cc8ea3cd800cd5c78
IjayAbby <inyangasi4@gmail.com>
2020-02-28 11:12:58 +0300

    Introduction to React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 //Creating a function component called pet
-const Pet = () => { 
+const Pet = (props) => { 
     return React.createElement("div",null[
-        React.createElement("h1",null,"Luna"),
-        React.createElement("h2",null,"Dog"),
-        React.createElement("h2",null,"Havanese")
+        React.createElement("h1",null,props.name),
+        React.createElement("h2",null,props.animal),
+        React.createElement("h2",null,props.breed)
     ])
 }
 
 const App = () => {
     return React.createElement("div",null,[
         React.createElement("h1",null,"Adopt Me"),
-        React.createElement(Pet)
+        React.createElement(Pet,{
+            name : "Luna",
+            animal :"Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet,{
+            name : "Pepper",
+            animal :"Hen",
+            breed: "Bird"
+        }),
+        React.createElement(Pet,{
+            name : "Doink",
+            animal :"Cat",
+            breed: "Mix"
+        })
     ])
 }
 ReactDOM.render(React.createElement(App),
