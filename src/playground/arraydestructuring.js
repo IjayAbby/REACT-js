0c08aec54e11f7c193105c78e1d0dc3cac0a59f8
IjayAbby <inyangasi4@gmail.com>
2020-03-02 09:56:21 +0300

    array destructuring using other variables

---
+++ src/playground/arraydestructuring.js
@@ -0,0 +1,6 @@
+var data = ['berlin','tokyo','pretoria','london']
+
+var [play,laugh,run,sport] = data; //These work as place holders to the data 
+                                   //instead of using index
+
+console.log(play);
