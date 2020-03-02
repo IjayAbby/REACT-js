f3bd597d0c8fd3f81d880b2cc8361454cbf71903
IjayAbby <inyangasi4@gmail.com>
2020-03-02 10:34:35 +0300

    added Animal menu

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,27 +1,45 @@
-import React,{ useState} from 'react';
-
+import React, { useState } from "react";
+import {ANIMALS} from '@frontendmasters/pet'
 
 const searchParams = () => {
-    //replace location 
-    //useState = Hooks
-const [location , updateLocation ] = useState("Seattle,WA")
+  //replace location
+  //useState = Hooks
+  //location is the current state
+  //updatelocation is the function to update
+  const [location, updateLocation] = useState("Seattle,WA");
+  const [animal, updateAnimal] = useState("");
+  
 
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor ="animal">
+          Animal
+          <select
+          id = "animal"
+          value = {animal}
+          onChange = {(e) => updateAnimal(e.target.value)}
+          onBlur = {(e) => updateAnimal(e.target.value)}
+          >
+            {ANIMALS.map(animal => {
+              return <option value = {animal}>{animal}</option>
+            })}
+          </select>
 
-    return (
-        <div className = "search-params">
-            <form>
-                <label htmlFor = "location">
-                    location
-                    <input id = "location" 
-                    value = {location} 
-                    placeholder = "Location"
-                    onChange = {(e)=>updateLocation(e.target.value)}
-                    />
-                </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
-}
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default searchParams;
